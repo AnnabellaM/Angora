2658551d7794ac3fac5275e5de5922df90bd15c6
AnnabellaM <annabellamiao@gmail.com>
2025-09-27 20:55:05 -0500

    logger

--- llvm_mode/dfsan_rt/dfsan/dfsan.cc
+++ llvm_mode/dfsan_rt/dfsan/dfsan.cc
@@ -23,7 +23,6 @@
 #include "../sanitizer_common/sanitizer_flag_parser.h"
 #include "../sanitizer_common/sanitizer_flags.h"
 #include "../sanitizer_common/sanitizer_libc.h"
-#include "../dfsan_interface.h"
 
 #include "defs.h"
 #include "dfsan.h"
